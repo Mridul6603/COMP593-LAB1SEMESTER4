df9590c81ae59599a3496bc815c5625534fa9c97
Mridul6603 <108378044+Mridul6603@users.noreply.github.com>
2023-01-24 21:20:54 -0500

    Completed New Feature
    -Not really an amazing feature but yet it has been added and now being committed to feature development.

--- script1again.py
+++ script1again.py
@@ -3,3 +3,5 @@ print("There should be one-- and perferably only one --obvious way to do it.")
 #comment 1 has been added to this script
 #another line from zen of python has been added in this script1
 print("Flat is better than nested")
+
+#new comment has been added for the new feature in part3
