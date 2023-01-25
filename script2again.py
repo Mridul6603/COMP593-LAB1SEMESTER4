0fbc1bd6fd3f11cbcac7437b49aec0bd7f83d116
Mridul6603 <108378044+Mridul6603@users.noreply.github.com>
2023-01-24 20:26:50 -0500

    Comments have been added back in script2.py
    Comment had been unintentionally deleted by revert. But now has been added with a follow up comment after that

--- script2again.py
+++ script2again.py
@@ -1,2 +1,7 @@
 print("Beautiful is better than ugly")
 print("Simple is better than complex")
+#comment added
+#another line from Zen of Python has been added to this script2
+print("Unless explicitly silenced")
+
+#this is a good programming advice, never listen to your boss!!
