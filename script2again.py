337f11190f1882a4ee30bc06b9f9f49ed3c97e04
Mridul6603 <108378044+Mridul6603@users.noreply.github.com>
2023-01-24 20:21:13 -0500

    Revert "Modified both the scripts in VS code"
    This reverts commit f1f0b30d570341f563a2efac528186ba4e893fb9.

--- script2again.py
+++ script2again.py
@@ -1,5 +1,2 @@
 print("Beautiful is better than ugly")
 print("Simple is better than complex")
-#comment added
-#another line from Zen of Python has been added to this script2
-print("Unless explicitly silenced")
