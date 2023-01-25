9c82fe0e1ee3da647c06c76b8f0033c0e88baf54
Mridul6603 <108378044+Mridul6603@users.noreply.github.com>
2023-01-24 20:16:43 -0500

    Added a comment script1.py

--- script1again.py
+++ script1again.py
@@ -1,2 +1,5 @@
 print("There should be one-- and perferably only one --obvious way to do it.")
 print("Although that way may not be obvious at first unless you're Dutch.")
+#comment 1 has been added to this script
+#another line from zen of python has been added in this script1
+print("Flat is better than nested")
